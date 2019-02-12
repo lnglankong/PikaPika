2ceb12ddb5d18f922f5717d8272b4df3cd97ff9d
Jessica <tubwong@ucdavis.edu>
2019-02-12 01:23:39 -0800

    Signup now updates the database

--- igApp/SignUp.js
+++ igApp/SignUp.js
@@ -7,14 +7,35 @@ import firebase from './Firebase.js'
 
 export default class SignUp extends React.Component {
   state = { email: '', password: '', errorMessage: null }
+  
+  //Constructor for new user in database
+  createUser(email){
+    firebase.database().ref('Users/').push({
+      'biography': '',  
+      email,
+      'first_name': '',
+      'last_name': '',
+      'profile_picture': ''
+    }).then((data)=>{
+      //success callback
+      console.log('data ' , data)
+    }).catch((error)=>{
+      //error callback
+      console.log('error ' , error)
+    })
+  }
+
+  // Adds email and password to firebase authentication
   handleSignUp = () => {
+    this.createUser(this.state.email)
     firebase
       .auth()
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then(() => this.props.navigation.navigate('Main'))
       .catch(error => this.setState({ errorMessage: error.message }))
   }
-render() {
+
+  render() {
     return (
       <View style={styles.container}>
         <Text>Sign Up</Text>
@@ -46,6 +67,7 @@ render() {
     )
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
