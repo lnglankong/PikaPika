9d66eb8c24f4669a56d11813234fe3ce451c1c43
Jessica <tubwong@ucdavis.edu>
2019-02-17 18:37:03 -0800

    Users now have unique usernames

--- igApp/SignUp.js
+++ igApp/SignUp.js
@@ -1,4 +1,3 @@
-
 // SignUp.js
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
@@ -6,33 +5,53 @@ import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 import firebase from './Firebase.js'
 
 export default class SignUp extends React.Component {
-  state = { email: '', password: '', errorMessage: null }
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '', 
+      email: '', 
+      password: '', 
+      errorMessage: null 
+    };
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.createUser = this.createUser.bind(this);
+  }
   
   //Constructor for new user in database
-  createUser(email){
-    firebase.database().ref('Users/').push({
+  createUser(username, email){
+    //Add user to User table with a unique key
+    const userRef = firebase.database().ref('Users/').push({
+      username,
       'biography': '',  
       email,
       'first_name': '',
       'last_name': '',
       'profile_picture': ''
-    }).then((data)=>{
-      //success callback
-      console.log('data ' , data)
-    }).catch((error)=>{
-      //error callback
-      console.log('error ' , error)
     })
+    //Add username to Username table
+    firebase.database().ref('Usernames/').update({[username]: userRef.key });
   }
 
   // Adds email and password to firebase authentication
-  handleSignUp = () => {
-    this.createUser(this.state.email)
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(() => this.props.navigation.navigate('Main'))
-      .catch(error => this.setState({ errorMessage: error.message }))
+  handleSignUp() {
+    const usersFound = firebase.database().ref('Usernames/').orderByKey().equalTo(this.state.username);
+    usersFound.once('value', (snapshot) => {
+      if (snapshot.val() !== null) {
+        // username already exists, ask user for a different name
+        console.log("username already exists");
+        this.setState({ errorMessage: "Username already exists" });
+      }
+      else {
+        //username does not yet exist, go ahead and add new user
+        console.log("username does not yet exist");
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.state.email, this.state.password)
+          .then(() => this.createUser(this.state.username, this.state.email))
+          .then(() => this.props.navigation.navigate('Main'))
+          .catch(error => this.setState({ errorMessage: error.message }))
+      }
+    })  
   }
 
   render() {
@@ -43,6 +62,13 @@ export default class SignUp extends React.Component {
           <Text style={{ color: 'red' }}>
             {this.state.errorMessage}
           </Text>}
+        <TextInput
+          placeholder="Username"
+          autoCapitalize="none"
+          style={styles.textInput}
+          onChangeText={username => this.setState({ username })}
+          value={this.state.username}
+        /> 
         <TextInput
           placeholder="Email"
           autoCapitalize="none"
