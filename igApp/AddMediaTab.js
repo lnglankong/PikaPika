3b74dc78435858af9d46548375480a7b049540a1
Jessica <tubwong@ucdavis.edu>
2019-02-20 00:24:25 -0800

    Add camera and gallery upload buttons (images not stored in db yet)

--- igApp/AddMediaTab.js
+++ igApp/AddMediaTab.js
@@ -1,11 +1,41 @@
-import React, {Component} from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import { ImagePicker, Permissions } from "expo";
 
 class AddMediaTab extends Component{
+  state = {
+    // TODO: Change this default image later
+    image: "https://firebasestorage.googleapis.com/v0/b/ecs165a.appspot.com/o/thomas.png?alt=media&token=db4789a0-8dc7-4b71-82a1-233708de941f",
+  };
+
+  selectPicture = async () => {
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+      aspect: 1,
+      allowsEditing: true,
+    });
+    if (!cancelled) {
+      this.setState({ image: uri });
+    }
+  }
+
+  takePicture = async () => {
+    await Permissions.askAsync(Permissions.CAMERA);
+    const { cancelled, uri } = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+    })
+    if (!cancelled) {
+      this.setState({ image: uri });
+    }
+  }
+  
   render(){
     return(
       <View style = {styles.container}>
         <Text>AddMediaTab</Text>
+        <Image style={styles.image} source={{uri: this.state.image}}/>
+        <Button title="Gallery" onPress={this.selectPicture} />
+        <Button title="Camera" onPress={this.takePicture} />
       </View>
     )
   }
@@ -19,5 +49,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    image: {
+      width: 300, 
+      height: 300,
+      backgroundColor: 'gray'
     }
 });
